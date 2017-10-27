96e9bd9630279640ce9b1129103a2be46f8cb4fd
Blair Davis <davis.blairrosalyn@gmail.com>
2017-10-27 15:02:52 -0700

    boolean logic

---
+++ little_programs/boolean_fun.py
@@ -0,0 +1,27 @@
+# boolean logic
+
+# AND logic
+bool_one = True and True
+bool_two = True and False
+bool_three = False and False
+bool_four = False and True
+
+# print the results
+print "AND boolean logic"
+print "True  /\  True  = " + str(bool_one)
+print "True  /\  False = " + str(bool_two)
+print "False /\  False = " + str(bool_three)
+print "False /\  True  = " + str(bool_four)
+
+# OR logic
+print "OR boolean logic"
+bool_one = True or True
+bool_two = True or False
+bool_three = False or False
+bool_four = False or True
+
+# print the results
+print "True  \/  True  = " + str(bool_one)
+print "True  \/  False = " + str(bool_two)
+print "False \/  False = " + str(bool_three)
+print "False \/  True  = " + str(bool_four)

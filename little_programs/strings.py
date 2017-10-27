02248656760f051126edff172a6b3fd33da649ff
Blair Davis <davis.blairrosalyn@gmail.com>
2017-10-27 14:28:59 -0700

    plays with basic string functionality

---
+++ little_programs/strings.py
@@ -0,0 +1,16 @@
+#set a string variable
+a_string = "word"
+print "The string is set to " + a_string
+
+#get length of string
+print "The length of the string is " + str(len(a_string))
+
+#make string uppercase
+print "The string in all uppercase is " + a_string.upper()
+
+#make string lowercase
+print "The string in all lowercase is " + a_string.lower()
+
+#get user input
+name = raw_input("What is your name? ")
+print"Your name is %s" % (name)

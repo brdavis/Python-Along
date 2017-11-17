488803f0b971d3b57b58f931773c14ad158e20f4
Blair Davis <davis.blairrosalyn@gmail.com>
2017-11-16 19:46:57 -0800

    calculates the squre root of a number

---
+++ little_programs/square_root.py
@@ -0,0 +1,8 @@
+# Program calculates the square root of a number
+
+# Collect number
+num = int(raw_input("Enter a number: "))
+
+# Print sqare root
+square_root = num ** 0.5
+print "The square root is: %.2f" % (square_root)

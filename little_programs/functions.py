5ed78ca0db640880a90277a884515a1c6b4579e6
Blair Davis <davis.blairrosalyn@gmail.com>
2017-11-17 16:22:47 -0800

    demonstrates functions in python

---
+++ little_programs/functions.py
@@ -0,0 +1,9 @@
+# This program demonstrates functions
+
+# This example says hello to different people
+def say_hello(greeting):
+	print greeting
+
+say_hello("Hi")
+say_hello("Hey you")
+say_hello("How are you?")

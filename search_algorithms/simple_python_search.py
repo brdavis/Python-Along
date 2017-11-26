8e272304f3d30055ef86ae147cba34f2e7239a73
Blair Davis <davis.blairrosalyn@gmail.com>
2017-11-26 15:57:33 -0800

    built in python method for search

---
+++ search_algorithms/simple_python_search.py
@@ -0,0 +1,11 @@
+# This program presents a simple search in python
+
+# Utilizing the built in 'in' operator to find an element in a list
+# Answer will be a boolean
+
+print (3 in [1,2,3])
+# will be True
+
+print (5 in [1,2,3])
+# will be False
+
